fa564aac30e37d7b155e7c29475847c707e0183f
Artem.P <artemprogofficial@gmail.com>
2020-10-06 11:04:20 +0100

    Amigoscourse Initilizer

---
+++ amigoscourse/src/test/java/com/course/amigoscourse/AmigoscourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.course.amigoscourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoscourseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
